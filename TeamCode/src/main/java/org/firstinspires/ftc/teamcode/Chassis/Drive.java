041a940fbc123de6d4c97192eb051d1a62917f50
NyquilDreams <ian.halim@students.tka.org>
2020-10-16 16:11:32 -0700

    Abstract Drive class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chassis/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chassis/Drive.java
@@ -1,6 +1,18 @@
 package org.firstinspires.ftc.teamcode.Chassis;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Common.Assembly;
 
 public abstract class Drive  extends Assembly {
+
+    public HardwareMap hardwareMap;
+    public Telemetry telemetry;
+
+    public abstract void start();
+    public abstract void stop();
+    public abstract void setSpeed(double speed);
+
 }
