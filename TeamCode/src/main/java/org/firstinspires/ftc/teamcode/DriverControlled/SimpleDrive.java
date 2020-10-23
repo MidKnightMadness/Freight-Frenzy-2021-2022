51e41ce8e247f391de803bc37f6ead47d28af8b3
juniperpeng <pengjupiter5@gmail.com>
2020-10-23 15:51:15 -0700

    fixed math in SimpleDrive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled/SimpleDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled/SimpleDrive.java
@@ -9,24 +9,24 @@ import org.firstinspires.ftc.teamcode.Common.Config;
 @TeleOp
 public class SimpleDrive extends OpMode {
 
-    private DcMotor TL;
-    private DcMotor TR;
+    private DcMotor FL;
+    private DcMotor FR;
     private DcMotor BL;
     private DcMotor BR;
 
     @Override
     public void init() {
-        TL = hardwareMap.dcMotor.get(Config.DRIVEFL);
-        TR = hardwareMap.dcMotor.get(Config.DRIVEFR);
+        FL = hardwareMap.dcMotor.get(Config.DRIVEFL);
+        FR = hardwareMap.dcMotor.get(Config.DRIVEFR);
         BL = hardwareMap.dcMotor.get(Config.DRIVEBL);
         BR = hardwareMap.dcMotor.get(Config.DRIVEBR);
     }
 
     @Override
     public void loop() {
-        TL.setPower(gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x);
-        TR.setPower(gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x);
+        FL.setPower(gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x);
+        FR.setPower(-gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x);
         BL.setPower(gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x);
-        BR.setPower(gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x);
+        BR.setPower(-gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x);
     }
 }
