78742813d5f53514a32719f317d6899b2fe1b462
juniperpeng <pengjupiter5@gmail.com>
2020-10-24 12:02:14 -0700

    changed SampleDrive to use Velocity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chassis/SampleDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chassis/SampleDrive.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Chassis;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -10,25 +11,26 @@ import org.firstinspires.ftc.teamcode.Common.Config;
 public class SampleDrive extends Drive{
 
     //declare wheel motors
-    private DcMotor motorFL;
-    private DcMotor motorFR;
-    private DcMotor motorBL;
-    private DcMotor motorBR;
+    private DcMotorEx motorFL;
+    private DcMotorEx motorFR;
+    private DcMotorEx motorBL;
+    private DcMotorEx motorBR;
+    private final double maxVel = 2500;
 
     @Override
     public void init(HardwareMap hardwareMap, Telemetry telemetry) {
         super.init(hardwareMap, telemetry);
-        motorFL = hardwareMap.dcMotor.get(Config.DRIVEFL);
-        motorFR = hardwareMap.dcMotor.get(Config.DRIVEFR);
-        motorBL = hardwareMap.dcMotor.get(Config.DRIVEBL);
-        motorBR = hardwareMap.dcMotor.get(Config.DRIVEBR);
+        motorFL = (DcMotorEx)hardwareMap.dcMotor.get(Config.DRIVEFL);
+        motorFR = (DcMotorEx)hardwareMap.dcMotor.get(Config.DRIVEFR);
+        motorBL = (DcMotorEx)hardwareMap.dcMotor.get(Config.DRIVEBL);
+        motorBR = (DcMotorEx)hardwareMap.dcMotor.get(Config.DRIVEBR);
     }
 
     @Override
     public void drive(double forwards, double sideways, double turn) {
-        motorFL.setPower(forwards + sideways + turn);
-        motorFR.setPower(forwards + sideways - turn);
-        motorBL.setPower(forwards - sideways + turn);
-        motorBR.setPower(forwards - sideways - turn);
+        motorFL.setVelocity((-forwards + sideways + turn) * maxVel);
+        motorFR.setVelocity((forwards + sideways - turn) * maxVel);
+        motorBL.setVelocity((-forwards - sideways + turn) * maxVel);
+        motorBR.setVelocity((forwards - sideways - turn) * maxVel);
     }
 }
