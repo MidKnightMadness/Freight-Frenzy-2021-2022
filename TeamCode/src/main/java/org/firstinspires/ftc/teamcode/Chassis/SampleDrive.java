302233bb4abd993bcc1bb91f0a72497397938dd1
juniperpeng <pengjupiter5@gmail.com>
2020-11-04 18:02:10 -0800

    added imu for getting the rotation of the robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chassis/SampleDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chassis/SampleDrive.java
@@ -1,10 +1,14 @@
 package org.firstinspires.ftc.teamcode.Chassis;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 import org.firstinspires.ftc.teamcode.Common.Assembly;
 import org.firstinspires.ftc.teamcode.Common.Config;
 import org.firstinspires.ftc.teamcode.Visual.Visual;
@@ -22,6 +26,8 @@ public class SampleDrive extends Drive{
     private final double maxVel = 2500;
     private Visual visual;
 
+    BNO055IMU imu;
+
     @Override
     public void init(HardwareMap hardwareMap, Telemetry telemetry) {
         super.init(hardwareMap, telemetry);
@@ -34,6 +40,28 @@ public class SampleDrive extends Drive{
         motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+
+        // Set up the parameters with which we will use our IMU. Note that integration
+        // algorithm here just reports accelerations to the logcat log; it doesn't actually
+        // provide positional information.
+        BNO055IMU.Parameters IMUParameters = new BNO055IMU.Parameters();
+        IMUParameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        IMUParameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        IMUParameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        IMUParameters.loggingEnabled      = true;
+        IMUParameters.loggingTag          = "IMU";
+        IMUParameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(IMUParameters);
+
+        // Start the logging of measured acceleration
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 100);
     }
 
     @Override
@@ -46,7 +74,7 @@ public class SampleDrive extends Drive{
 
     @Override
     public void moveToPosition(double x, double y) {
-        double angle = visual.getRotation().thirdAngle;
+        double angle = imu.getAngularOrientation().thirdAngle;
         VectorF currentPosition = visual.getPosition();
         double positionX = currentPosition.get(0);
         double positionY = currentPosition.get(2);
@@ -54,11 +82,11 @@ public class SampleDrive extends Drive{
         //turn bot until facing field's positive y-axis
         while(angle > 0){
             drive(0,0,-1);
-            angle = visual.getRotation().thirdAngle;
+            angle = imu.getAngularOrientation().thirdAngle;
         }
         while(angle < 0){
             drive(0,0,1);
-            angle = visual.getRotation().thirdAngle;
+            angle = imu.getAngularOrientation().thirdAngle;
         }
 
         //while the target is in the I/IV Quadrant in relation to the bot, turn clockwise until target is on bot's x-axis or y-axis
