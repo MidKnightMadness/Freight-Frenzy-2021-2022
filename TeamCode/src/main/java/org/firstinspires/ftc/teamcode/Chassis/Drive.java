8e47399518124eb9d1c4a4996a2bab3db3d9df54
NyquilDreams <ian.halim@students.tka.org>
2020-11-04 16:18:56 -0800

    moveToPosition(VectorF) finished probably

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chassis/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chassis/Drive.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.teamcode.Common.Assembly;
 
 public abstract class Drive extends Assembly {
@@ -11,5 +12,5 @@ public abstract class Drive extends Assembly {
     public abstract void drive(double forwards, double sideways, double turn);
 
     public abstract void moveToPosition(double x, double y);
-
+    public abstract void moveToPosition(VectorF target);
 }
