3ccb421f5f28c78c202178d616c95f9e5c0c9f9d
juniperpeng <pengjupiter5@gmail.com>
2020-11-07 11:58:59 -0800

    fixed directions of motors added encoderticks to telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled/SimpleDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled/SimpleDrive.java
@@ -25,13 +25,31 @@ public class SimpleDrive extends OpMode {
         FR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        FL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     @Override
     public void loop() {
-        FL.setPower(-gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x);
-        FR.setPower(gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x);
+        FL.setPower(gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x);
+        FR.setPower(-gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x);
         BL.setPower(-gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x);
         BR.setPower(gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x);
+
+//        telemetry.addData("FL vel", FL.getVelocity());
+//        telemetry.addData("FR vel", FR.getVelocity());
+//        telemetry.addData("BL vel", BL.getVelocity());
+//        telemetry.addData("BR vel", BR.getVelocity());
+        telemetry.addData("FL pos", FL.getCurrentPosition());
+        telemetry.addData("FR pos", FR.getCurrentPosition());
+        telemetry.addData("BL pos", BL.getCurrentPosition());
+        telemetry.addData("BR pos", BR.getCurrentPosition());
     }
 }
