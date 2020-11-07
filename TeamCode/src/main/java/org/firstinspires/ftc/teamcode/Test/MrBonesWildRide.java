4e902f9df4a077686af6b93723d02adc472f62e7
NyquilDreams <ian.halim@students.tka.org>
2020-11-07 11:11:39 -0800

    fixed moveToPosition to not require camera (hopefully)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/MrBonesWildRide.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.Test;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Chassis.Drive;
+import org.firstinspires.ftc.teamcode.Chassis.SampleDrive;
+
+@Autonomous
+public class MrBonesWildRide extends LinearOpMode {
+    private Drive drive = new SampleDrive();
+
+    @Override
+    public void runOpMode() {
+        drive.init(hardwareMap, telemetry);
+        waitForStart();
+
+        drive.moveToPosition(1,1);
+    }
+
+}
