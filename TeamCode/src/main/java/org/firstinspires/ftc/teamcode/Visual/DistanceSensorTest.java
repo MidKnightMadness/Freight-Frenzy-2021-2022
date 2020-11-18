deca6ac3691fd615b5fb4788a775981d1b56ccb2
juniperpeng <pengjupiter5@gmail.com>
2020-11-17 17:28:31 -0800

    added front left and right distance sensors to DistanceSensorTest made DistanceSensorDriveTest align both front and right

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Visual/DistanceSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Visual/DistanceSensorTest.java
@@ -5,22 +5,29 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Common.Config;
 
 @TeleOp
 public class DistanceSensorTest extends OpMode {
 
     //sensor declaration
-    private Rev2mDistanceSensor distanceSensor;
+    private Rev2mDistanceSensor distF;
+    private Rev2mDistanceSensor distL;
+    private Rev2mDistanceSensor distR;
 
     @Override
     public void init()
     {
-        distanceSensor = hardwareMap.get(Rev2mDistanceSensor.class, "distanceSensor");
+        distF = hardwareMap.get(Rev2mDistanceSensor.class, Config.DISTANCESENSORFRONT);
+        distL = hardwareMap.get(Rev2mDistanceSensor.class, Config.DISTANCESENSORLEFT);
+        distR = hardwareMap.get(Rev2mDistanceSensor.class, Config.DISTANCESENSORRIGHT);
     }
 
     @Override
     public void loop()
     {
-        telemetry.addData("Distance from wall (in.): ", distanceSensor.getDistance(DistanceUnit.INCH));
+        telemetry.addData("DistF", distF.getDistance(DistanceUnit.INCH));
+        telemetry.addData("DistL", distL.getDistance(DistanceUnit.INCH));
+        telemetry.addData("DistR", distR.getDistance(DistanceUnit.INCH));
     }
 }
