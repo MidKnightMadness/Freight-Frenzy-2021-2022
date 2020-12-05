0590e6eeaf6f290c4e31bfc712caf064ff3b8e14
NyquilDreams <ian.halim@students.tka.org>
2020-12-05 13:21:20 -0800

    adjusted autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoRedRightManual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoRedRightManual.java
@@ -73,7 +73,7 @@ public class AutoRedRightManual extends LinearOpMode {
         if (visual.getStartStack() == Visual.STARTERSTACK .A) {
             drive.move(-5, 58);
         } else if (visual.getStartStack() == Visual.STARTERSTACK.B) {
-            drive.move(-15, 78);
+            drive.move(-10, 80);
         } else {
             drive.move(-5, 100);
         }
