120ba5fc0d9fa4538ffdeeefeb686e39de3152d1
NyquilDreams <ian.halim@students.tka.org>
2021-01-19 16:30:34 -0800

    autonomous powershots baby

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoRedRightManual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoRedRightManual.java
@@ -94,15 +94,15 @@ public class AutoRedRightManual extends LinearOpMode {
 
         //move to shooting position 3
         if (visual.getStartStack() == Visual.STARTERSTACK.A) {
-            drive.move(-17.25, -25);
+            drive.move(-11.25, -25);
             telemetry.addLine("Zone: " + visual.getStartStack());
             telemetry.update();
         } else if (visual.getStartStack() == Visual.STARTERSTACK.B) {
-            drive.move(8.25, -47);
+            drive.move(14.25, -47);
             telemetry.addLine("Zone: " + visual.getStartStack());
             telemetry.update();
         } else {
-            drive.move(-17.25, -72);
+            drive.move(-11.25, -72);
             telemetry.addLine("Zone: " + visual.getStartStack());
             telemetry.update();
         }
@@ -130,7 +130,7 @@ public class AutoRedRightManual extends LinearOpMode {
         outtake.feedRun();
         sleep(1000);
         outtake.resetFeed();
-        sleep(1000);
+            sleep(1000);
         outtake.stop();
         drive.move(0, 12);
 
