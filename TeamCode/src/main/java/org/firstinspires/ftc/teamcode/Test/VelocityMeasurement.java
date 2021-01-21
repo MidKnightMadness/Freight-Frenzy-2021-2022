cef28221f0c66fa8ac9022bf2bac264b946f7297
juniperpeng <pengjupiter5@gmail.com>
2021-01-20 18:04:12 -0800

    wrote code to measure velocity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/VelocityMeasurement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/VelocityMeasurement.java
@@ -4,12 +4,14 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+import org.firstinspires.ftc.teamcode.Common.Config;
+
 @TeleOp
 public class VelocityMeasurement extends OpMode {
     DcMotorEx motor;
     @Override
     public void init() {
-        motor = (DcMotorEx) hardwareMap.dcMotor.get("motor");
+        motor = (DcMotorEx) hardwareMap.dcMotor.get(Config.OUTTAKEMOTOR);
     }
 
     @Override
