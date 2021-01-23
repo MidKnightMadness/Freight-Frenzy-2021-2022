8b66d1f5b60da3b4c42b348f9d0b0532986b7b32
NyquilDreams <ian.halim@students.tka.org>
2021-01-23 09:19:45 -0800

    fixed bot not stopping when we press the stop button?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoRedRightManual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoRedRightManual.java
@@ -30,9 +30,9 @@ public class AutoRedRightManual extends LinearOpMode {
     @Override
     public void runOpMode() {
         //create stop requested callable
-        //Callable<Boolean> stopRequestedCall = new Callable<Boolean>() {@Override public Boolean call() {return isStopRequested();}};
+        Callable<Boolean> stopRequestedCall = new Callable<Boolean>() {@Override public Boolean call() {return isStopRequested();}};
         drive.init(hardwareMap, telemetry, gamepad1, gamepad2);
-//        drive.isStopRequested = stopRequestedCall;
+        drive.isStopRequested = stopRequestedCall;
         telemetry.addLine("Drive initialized!");
         telemetry.update();
         intake.init(hardwareMap, telemetry, gamepad1, gamepad2);
