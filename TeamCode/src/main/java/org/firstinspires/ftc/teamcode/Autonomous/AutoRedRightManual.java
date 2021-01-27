f207d8bdc385a28a3c1e8c739d3c00a82d7a902b
NyquilDreams <ian.halim@students.tka.org>
2021-01-26 16:09:00 -0800

    fixed power shot positions?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoRedRightManual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoRedRightManual.java
@@ -89,15 +89,15 @@ public class AutoRedRightManual extends LinearOpMode {
 
         //move to shooting position 3
         if (visual.getStartStack() == Visual.STARTERSTACK.A) {
-            drive.move(-30.25, -21);
+            drive.move(-24.25, -21);
             telemetry.addLine("Zone: " + visual.getStartStack());
             telemetry.update();
         } else if (visual.getStartStack() == Visual.STARTERSTACK.B) {
-            drive.move(-4.75, -43);
+            drive.move(1.25, -43);
             telemetry.addLine("Zone: " + visual.getStartStack());
             telemetry.update();
         } else {
-            drive.move(-30.25, -68);
+            drive.move(-24.25, -68);
             telemetry.addLine("Zone: " + visual.getStartStack());
             telemetry.update();
         }
