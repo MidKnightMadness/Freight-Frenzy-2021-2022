f008ecb78860b3ad96fc8351c31cbe7da0f1f0ad
NyquilDreams <ian.halim@students.tka.org>
2021-02-11 16:55:10 -0800

    AutoRedRightTurn works, turn angles just need to be adjusted

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoRedRightTurn.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoRedRightTurn.java
@@ -105,15 +105,15 @@ public class AutoRedRightTurn extends LinearOpMode {
 
         //move to shooting position 3
         if (visual.getStartStack() == Visual.STARTERSTACK.A) {
-            drive.move(-15, -21);
+            drive.move(-14, -21);
             telemetry.addLine("Zone: " + visual.getStartStack());
             telemetry.update();
         } else if (visual.getStartStack() == Visual.STARTERSTACK.B) {
-            drive.move(25, -43);
+            drive.move(26, -43);
             telemetry.addLine("Zone: " + visual.getStartStack());
             telemetry.update();
         } else {
-            drive.move(-15, -68);
+            drive.move(-14, -68);
             telemetry.addLine("Zone: " + visual.getStartStack());
             telemetry.update();
         }
@@ -129,9 +129,9 @@ public class AutoRedRightTurn extends LinearOpMode {
         telemetry.addData("Current Y: ", drive.getCurrentY());
         telemetry.update();
 
-        drive.adjustWalls(60, 40);
+        drive.adjustWalls(60, 39);
         sleep(1);
-        drive.adjustWalls(60, 40);
+        drive.adjustWalls(60, 39);
         drive.setCurrentY(drive.getCurrentY() + 18);
 
         telemetry.addData("DistF: ", drive.getDistF());
@@ -160,14 +160,14 @@ public class AutoRedRightTurn extends LinearOpMode {
         sleep(1000);
 
         //drive.turnToPoint(-31.5,144);
-        drive.betterTurn(5);
+        drive.betterTurn(4);
         outtake.feedRun();
         sleep(1000);
         outtake.resetFeed();
         sleep(1000);
 
         //drive.turnToPoint(-39,144);
-        drive.betterTurn(5);
+        drive.betterTurn(6);
         outtake.feedRun();
         sleep(1000);
         outtake.resetFeed();
