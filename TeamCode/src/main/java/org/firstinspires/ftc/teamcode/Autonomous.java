3f19fbd8c33122fc09f149a3f03320831dabf2de
mchan422 <85671225+mchan422@users.noreply.github.com>
2021-11-03 17:40:04 -0700

    Changed power to drive to alliance shipping hub

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -14,7 +14,7 @@ public class Autonomous extends LinearOpMode {
 
         // Detecting Team Shipping Element and Placing Pre-Load Box (7 seconds)
         drive.drive(-1, 0.8, 0); //drive to alliance shipping hub
-        sleep(800);
+        sleep(1200);
 
         drive.drive(0,0,0);
         sleep(3000);
@@ -23,13 +23,12 @@ public class Autonomous extends LinearOpMode {
         //drive.drive(0,0,-1); //drive to carousel from shipping hub
         //sleep(1000);
         drive.drive(-1,-0.5,0);
+        sleep(2200);
+        drive.drive(0,0,0);
         sleep(2000);
 
-        drive.drive(0,0,0);
-        sleep(3000);
-
-        // Placing Duck on Alliance Shipping Hub
-        /*drive.drive(0,0,-1); //drive to alliance shipping hub from carousel
+         //Placing Duck on Alliance Shipping Hub
+        drive.drive(0,0,-1); //drive to alliance shipping hub from carousel
         sleep(2000);
         drive.drive(0,1,0);
         sleep(2000);
@@ -42,7 +41,7 @@ public class Autonomous extends LinearOpMode {
 
         drive.drive(0,0,-1); //drive to alliance shipping hub from warehouse
         sleep(200);
-        drive.drive(0,1,0);
+        drive.drive(1,1,0);
         sleep(1000);
 
         // Completely Parking in Warehouse (4 seconds)
