87cbe34d1be5191ac1be25625fc30d670dc22ec0
redpotato <50560736+redpotat0@users.noreply.github.com>
2021-11-03 19:32:36 -0700

    Adjusted time and power to drive to alliance shipping hub, carousel, back to shipping hub, and to warehouse.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -17,24 +17,24 @@ public class Autonomous extends LinearOpMode {
         sleep(1200);
 
         drive.drive(0,0,0);
-        sleep(3000);
+        sleep(1000);
 
         // Deliver Duck Through Carousel (5 seconds)
         //drive.drive(0,0,-1); //drive to carousel from shipping hub
         //sleep(1000);
-        drive.drive(-1,-0.5,0);
-        sleep(2200);
+        drive.drive(-1,-0.2,0);
+        sleep(2400);
         drive.drive(0,0,0);
-        sleep(2000);
+        sleep(1000);
 
          //Placing Duck on Alliance Shipping Hub
-        drive.drive(0,0,-1); //drive to alliance shipping hub from carousel
-        sleep(2000);
-        drive.drive(0,1,0);
-        sleep(2000);
+        drive.drive(1,0.2,0); //drive to alliance shipping hub from carousel
+        sleep(2500);
+        drive.drive(0,0,0);
+        sleep(1000);
 
         // Placing 2 Freight from Warehouse to Alliance Shipping Hub (10 seconds)
-        drive.drive(0,0,1); //drive to warehouse from alliance shipping hub
+        drive.drive(-1, 0.8, 0); //drive to warehouse from alliance shipping hub
         sleep(500);
         drive.drive(0,1,0);
         sleep(1000);
