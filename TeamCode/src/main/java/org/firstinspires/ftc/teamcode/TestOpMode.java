ebe0333fddabc923f05c766799254a658595c0ab
redpotato <50560736+redpotat0@users.noreply.github.com>
2021-11-05 15:22:08 -0700

    Renamed file TestOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
 
-public class testOpMode extends OpMode {
+public class TestOpMode extends OpMode {
     SampleDrive drive;
 
     @Override
