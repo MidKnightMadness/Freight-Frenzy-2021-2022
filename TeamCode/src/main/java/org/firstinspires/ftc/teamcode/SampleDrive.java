9b92f50f77032ae97b5215866979ee1273d56457
redpotato <50560736+redpob@users.noreply.github.com>
2022-01-08 14:20:50 -0800

    Testopmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleDrive.java
@@ -1,10 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
-
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class SampleDrive {
     //DcMotor FRMotor;
@@ -17,6 +21,10 @@ public class SampleDrive {
     DcMotorEx BRMotor;
     DcMotorEx BLMotor;
 
+    private DistanceSensor sensorRangeL; //left front sensor
+    ModernRoboticsI2cRangeSensor sensorRangeM;
+    private DistanceSensor sensorRangeR; //right front sensor
+
     public SampleDrive(HardwareMap hardwareMap) {
         //FRMotor = hardwareMap.dcMotor.get("FR");
         //FLMotor = hardwareMap.dcMotor.get("FL");
@@ -27,18 +35,57 @@ public class SampleDrive {
         FLMotor = hardwareMap.get(DcMotorEx.class, "FL");
         BRMotor = hardwareMap.get(DcMotorEx.class, "BR");
         BLMotor = hardwareMap.get(DcMotorEx.class, "BL");
+
+        sensorRangeL = hardwareMap.get(DistanceSensor.class, "sensor_range_left");
+        sensorRangeM = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "sensor_range_middle");
+        sensorRangeR = hardwareMap.get(DistanceSensor.class, "sensor_range_right");
     }
 
-    public void drive(double xx, double yy, double rotation) {
-        //FRMotor.setPower(xx + yy + rotation);
-        //FLMotor.setPower(xx - yy + rotation);
-        //BRMotor.setPower(-xx + yy + rotation);
-        //BLMotor.setPower(-xx - yy + rotation);
+    public void drive(double x, double y, double rotation) {
+        //FRMotor.setPower(x + y + rotation);
+        //FLMotor.setPower(x - y + rotation);
+        //BRMotor.setPower(-x + y + rotation);
+        //BLMotor.setPower(-x - y + rotation);
 
-        FRMotor.setVelocity((xx - yy + rotation) * 1000);
-        FLMotor.setVelocity((xx + yy + rotation) * 1000);
-        BRMotor.setVelocity((-xx - yy + rotation) * 1000);
-        BLMotor.setVelocity((-xx + yy + rotation) * 1000);
+        FRMotor.setVelocity((x - y + rotation) * 1000);
+        FLMotor.setVelocity((x + y + rotation) * 1000);
+        BRMotor.setVelocity((-x - y + rotation) * 1000);
+        BLMotor.setVelocity((-x + y + rotation) * 1000);
+    }
 
+    public void setPos(double x, double y, double rotation) {
+        FRMotor.setTargetPosition((int)( x - y + rotation) + FRMotor.getCurrentPosition());
+        FLMotor.setTargetPosition((int)( x + y + rotation) + FLMotor.getCurrentPosition());
+        BRMotor.setTargetPosition((int)(-x - y + rotation) + BRMotor.getCurrentPosition());
+        BLMotor.setTargetPosition((int)(-x + y + rotation) + BLMotor.getCurrentPosition());
+        FRMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FRMotor.setPower(1.0);
+        FLMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FLMotor.setPower(1.0);
+        BRMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BRMotor.setPower(1.0);
+        BLMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BLMotor.setPower(1.0);
+        while(!atTarget()) { }
+    }
+
+    public boolean atTarget() {
+        if(FRMotor.getCurrentPosition() > FRMotor.getTargetPosition() - 10 && FRMotor.getCurrentPosition() < FRMotor.getTargetPosition() + 10 &&
+                FLMotor.getCurrentPosition() > FLMotor.getTargetPosition() - 10 && FLMotor.getCurrentPosition() < FLMotor.getTargetPosition() + 10 &&
+                BRMotor.getCurrentPosition() > BRMotor.getTargetPosition() - 10 && BRMotor.getCurrentPosition() < BRMotor.getTargetPosition() + 10 &&
+                BLMotor.getCurrentPosition() > BLMotor.getTargetPosition() - 10 && BLMotor.getCurrentPosition() < BLMotor.getTargetPosition() + 10)
+            return true;
+        else
+            return false;
+    }
+
+    public void telemetry(Telemetry telemetry) {
+        telemetry.addData("FR Motor Position", FRMotor.getCurrentPosition());
+        telemetry.addData("FL Motor Position", FLMotor.getCurrentPosition());
+        telemetry.addData("BR Motor Position", BRMotor.getCurrentPosition());
+        telemetry.addData("BL Motor Position", BLMotor.getCurrentPosition());
+        telemetry.addData("deviceName", sensorRangeM.getDeviceName());
+        telemetry.addData("Left Sensor Range", String.format("%.01f in", sensorRangeM.getDistance(DistanceUnit.INCH)));
+        telemetry.update();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -1,20 +1,41 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorMRRangeSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @TeleOp
 
 public class TestOpMode extends OpMode {
     SampleDrive drive;
 
+    private DistanceSensor sensorDistanceL; //left front sensor
+    private ModernRoboticsI2cRangeSensor sensorRangeM; //left front sensor
+    private DistanceSensor sensorDistanceR; //right front sensor
+
     @Override
     public void init() {
         drive = new SampleDrive(hardwareMap);
+        sensorDistanceL = hardwareMap.get(DistanceSensor.class, "sensor_distance_left");
+        sensorRangeM = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "sensor_range_middle");
+        sensorDistanceR = hardwareMap.get(DistanceSensor.class, "sensor_distance_right");
     }
 
     @Override
     public void loop() {
-        drive.drive(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x);
+        //drive to shipping hub position
+        if((sensorRangeM.getDistance(DistanceUnit.INCH) <= 9 || sensorRangeM.getDistance(DistanceUnit.INCH) >= 11) && gamepad1.a) {
+            drive.drive(0, (sensorRangeM.getDistance(DistanceUnit.INCH) - 10)/100, 0);
+        } else {
+            drive.drive(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x);
+            drive.telemetry(telemetry);
+        }
+        telemetry.addData("deviceName", sensorRangeM.getDeviceName());
+        telemetry.addData("Left Sensor Range", String.format("%.01f in", sensorRangeM.getDistance(DistanceUnit.INCH)));
+        telemetry.update();
     }
 }
