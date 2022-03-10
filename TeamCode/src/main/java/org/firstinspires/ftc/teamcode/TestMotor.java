ed553c03585472a0e7576787ec25b62118e229b2
redpotato <50560736+redpob@users.noreply.github.com>
2022-03-09 20:18:34 -0800

    Testing catapult motor and flap

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMotor.java
@@ -13,19 +13,31 @@ public class TestMotor extends OpMode {
     private DcMotorEx catapultMotor;
     private int i;
     private int startPosition;
+    private Servo flapServo;
 
     public void init() {
         catapultMotor = hardwareMap.get(DcMotorEx.class, "catapult");
+        flapServo = hardwareMap.get(Servo.class, "flap");
         catapultMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         startPosition = catapultMotor.getCurrentPosition();
-        i = 1000;
+        i = 0;
     }
 
     public void loop() {
         if(gamepad1.dpad_up)
             i++;
+        if(gamepad1.dpad_down)
+            i--;
+        if (gamepad1.x) {
+            flapServo.setPosition(0);
+        }
+        if (gamepad1.y) {
+            flapServo.setPosition(0.5);
+        }
 
         catapultMotor.setTargetPosition(startPosition  + i);
+        catapultMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        catapultMotor.setPower(1);
         telemetry.addData("Catapult Motor Current", catapultMotor.getCurrentPosition());
         telemetry.addData("Catapult Motor Target", catapultMotor.getTargetPosition());
         telemetry.addData("i", i);
